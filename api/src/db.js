5cb88d34986d50b1989321dc32a9a1078cbb2a6c
userJAMT <jmendez4tob@gmail.com>
2022-09-24 21:44:01 -0300

    models done

--- api/src/db.js
+++ api/src/db.js
@@ -37,12 +37,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
-//const {Tipo} = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
 
 // Aca vendrian las relaciones
-//Pokemon.belongsToMany(Tipo, 'Pokemon-Tipo');
-//Tipo.belongsToMany(Pokemon, 'Pokemon-Tipo');
+Pokemon.belongsToMany(Type, {through: 'pokemons_types'});
+Type.belongsToMany(Pokemon, {through: 'pokemons_types'});
 
 
 
