e04977cf038cb914cbe2f31619a711edcb97f4bf
userJAMT <jmendez4tob@gmail.com>
2023-01-20 14:18:13 -0300

    Adding PORT for public domain

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require("./src/app.js");
 const { getPokemonsInApi, getTypesInApi } = require("./src/controllers/api.js");
 const { conn, Pokemon, Type } = require("./src/db.js");
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
   try {
     let types = await Type.findAll();
